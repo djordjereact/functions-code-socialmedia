aa49655c29a33f20b106371b9da246133e6c34fe
djordjereact <djole07mihailovic@gmail.com>
2020-05-20 21:44:32 +0200

    Finished with add and get user details

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+    users: [
+        {
+            userId: '1564fas465df454a9',
+            email: 'user@email.com',
+            handle: 'user',
+            createdAt: '2020-05-20-20:04:10',
+            imageUrl: 'image/dfdlkkjhfanjklf/bfjkalbkfja',
+            bio: 'Hello, my name is user, nice to meet you',
+            website: 'https://user.com',
+            location: 'London, UK'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
@@ -6,6 +18,29 @@ let db = {
             createdAt: '2020-05-18T18:58:58.203Z',
             likeCount: 5,
             commentCount: 2
+        }
+    ]
+};
+const userDetails = {
+    // Redux data
+    credentials: {
+        userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+        email: 'user@email.com',
+        handle: 'user',
+        createdAt: '2020-05-20T21:32:40.798Z',
+        imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+        bio: 'Hello, my name is user, nice to meet you',
+        website: 'https://user.com',
+        location: 'Lonodn, UK'
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'hh7O5oWfWucVzGbHH2pa'
+        },
+        {
+            userHandle: 'user',
+            screamId: '3IOnFoQexRcofs5OhBXO'
         }
     ]
 };
