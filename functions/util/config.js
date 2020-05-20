1b909f0ce90103df2eb2fb19dfbfcb9a2533d072
djordjereact <djole07mihailovic@gmail.com>
2020-05-20 17:33:43 +0200

    Organising my code finally

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAMFjR-jr4fBauzeLSbzK21WRv8QWZgL4Q",
+    authDomain: "social-app-aca12.firebaseapp.com",
+    databaseURL: "https://social-app-aca12.firebaseio.com",
+    projectId: "social-app-aca12",
+    storageBucket: "social-app-aca12.appspot.com",
+    messagingSenderId: "463270189467",
+    appId: "1:463270189467:web:83d2afce13ff5f2088293e",
+    measurementId: "G-194NYS5K5V"
+};
