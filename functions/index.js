515a42b5e8a1d3581dfe4b32c88432c1a9e6c4fb
djordjereact <djole07mihailovic@gmail.com>
2020-05-21 19:02:28 +0200

    I did this post comment one time, but it doesn`t work, but now it`s working

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,8 @@ const FBAuth = require('./util/FBAuth');
 const {
     getAllScreams,
     postOneScream,
-    getScream
+    getScream,
+    commentOnScream
 } = require('./handlers/screams');
 const {
     signup,
@@ -22,7 +23,7 @@ app.get('/scream/:screamId', getScream);
 //TODO: delete scream
 //TODO: like a scream
 //TODO: unlike scream
-//TODO: comment on scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 //Users routes
 app.post('/signup', signup);
